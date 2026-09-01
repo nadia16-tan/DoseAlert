bf62314b4ba29d5bed53cfbe791a1bb8c850869d
Nadia Nyamanyunzo <nanyajhb025@student.wethinkcode.co.za>
2026-09-01 11:14:43 +0200

    feat:Added app route

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 @app.route("/")
 
 def home():
     return render_template("index.html")
 app.run()
+
+
+@app.route("/set-reminder", methods=["POST"])
+def set_reminder():
+    medication = request.form["medication"]
+    
+app.run()
+
     
 
