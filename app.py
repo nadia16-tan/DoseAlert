7b907358e3e5b666fb2e3824b497ad495ad469ac
Nadia Nyamanyunzo <nanyajhb025@student.wethinkcode.co.za>
2026-09-12 01:18:44 +0200

    feat:connected to lambda

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template,request
 import sqlite3
+import boto3
 app = Flask(__name__)
+dynamodb = boto3.resource("dynamodb", region_name="eu-north-1")
+table = dynamodb.Table("MedicationSchedules")
+ses = boto3.client("ses", region_name="eu-north-1")
 
 def init_db():
     connection = sqlite3.connect("reminders.db")
@@ -8,6 +12,7 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS reminders(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
             medication TEXT NOT NULL,
             email TEXT NOT NULL,
             reminder_time TEXT NOT NULL
@@ -31,6 +36,7 @@ def home():
 
 @app.route("/set-reminder", methods=["POST"])
 def set_reminder():
+    name = request.form["name"]
     medication = request.form["medication"]
     email= request.form["email"]
     reminder= request.form["reminder_time"]
@@ -40,13 +46,23 @@ def set_reminder():
     cursor=connection.cursor()
     
     cursor.execute("""
-        INSERT INTO reminders (medication, email, reminder_time)
-        VALUES(?, ?, ?)
-    """,(medication, email, reminder))     
+        INSERT INTO reminders (name, medication, email, reminder_time)
+        VALUES(?, ?, ?, ?)
+    """,(name, medication, email, reminder))     
     
     connection.commit()
 
-    connection.close()      
+    connection.close()   
+    
+    table.put_item(
+        Item={
+            "user_id": email,
+            "name": name,
+            "email": email,
+            "medication_name": medication,
+            "schedule_times": [reminder]
+        }
+    )   
     
     return "reminder saved successfully"
     
